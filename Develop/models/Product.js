badb9cc6fb6935ee6a87b0d9925b1d945660eee6
Peter Kim <peter.kim@uconn.edu>
2022-11-04 02:10:55 -0400

    product.js category_id added with integer datatype and references category.id

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -34,6 +35,14 @@ Product.init(
       defaultValue: 10,
       isNumeric: true
     },
+    // category_id - given integer value, references category.id
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
