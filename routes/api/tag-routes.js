3118df6ae26218f0e28dff887bfa5502cf323c63
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-11-04 22:04:17 -0400

    promisified tag-routes.js routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,11 +3,11 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = Tag.findAll({
+    const tagData = await Tag.findAll({
       include: [{model: Product}],
     });
     res.status(200).json(tagData);
@@ -17,11 +17,11 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = Tag.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{model: Product}],
     });
     res.status(200).json(tagData);
@@ -31,10 +31,10 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = Tag.create({
+    const tagData = await Tag.create({
       tag_id: req.body.tag_id,
     });
     res.status(200).json(tagData);
@@ -44,10 +44,10 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = Tag.update({
+    const tagData = await Tag.update({
       where: {
         id: req.params.id,
       }
@@ -59,10 +59,10 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = Tag.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       },
