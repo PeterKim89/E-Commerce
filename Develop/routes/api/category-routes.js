318ca53ac1b1e644c3861207f47db3ee49e7a953
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-11-04 20:48:57 -0400

    category-routes - delete route deletes a category.id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -61,6 +61,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
