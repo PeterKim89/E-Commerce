199fde487c026abaf3038e2775d53581e797cd9f
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-11-04 21:08:44 -0400

    tag-routes - get(:id) route returns specified tag with associated product data

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -20,6 +20,15 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = Tag.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
