2de7a98e534cb20fa52c260610baeedec7a4f302
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-11-04 21:04:58 -0400

    product-routes - delete(:id) deletes a specified product.id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -109,6 +109,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(productData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
