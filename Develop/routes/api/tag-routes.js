74efc1a3d17da84b5f1347a58005cc4cad84f46b
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-11-04 21:17:44 -0400

    tag-routes - delete route deletes a specified tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -61,6 +61,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
