7a093dac556decd8670811619ac03045bd1b80d3
Peter Kim <peter.kim@uconn.edu>
2022-11-05 02:31:31 -0400

    categories put route now updates model correctly

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,11 +47,10 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update({
-      where: {
-        id: req.params.id,
-      }
-    });
+    const categoryData = await Category.update(
+      {category_name: req.body.category_name},
+      {where: {id: req.params.id}}
+    );
     res.status(200).json(categoryData);
   }
   catch (err) {
